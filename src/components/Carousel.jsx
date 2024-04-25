12c6fc931494f55eeba18e661a0d196c35a1949c
nikunj309 <103521892+nikunj309@users.noreply.github.com>
2024-04-25 15:27:12 +0530

    change carousel color

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -10,14 +10,22 @@ import im4 from "../assets/img1/B4.png"
 // import im2 from "../assets/img/home-slide-2.jpg"
 // import im3 from "../assets/img/home-slide-3.jpg"
 const CarouselComponent = () => {
- return (
+  return (
     <Carousel>
       <Carousel.Item>
-        <img
+        <div className="carousel-image-container">
+          <img
+            className="d-block w-100"
+            src={im1}
+            alt="Carousel Image"
+          />
+          <div className="carousel-image-overlay"></div>
+        </div>
+        {/* <img
           className="d-block w-100"
           src={im1}
           alt="Carousel Image"
-        />
+        /> */}
         <Carousel.Caption>
           <p className="animated fadeInRight" >We Are Professional</p>
           <h1 className="animated fadeInLeft">For Your Dream Projects</h1>
@@ -25,11 +33,14 @@ const CarouselComponent = () => {
       </Carousel.Item>
 
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={im2}
-          alt="Carousel Image"
-        />
+        <div className="carousel-image-container">
+          <img
+            className="d-block w-100"
+            src={im2}
+            alt="Carousel Image"
+          />
+          <div className="carousel-image-overlay"></div>
+        </div>
         <Carousel.Caption>
           <p className="animated fadeInRight">Professional Builder</p>
           <h1 className="animated fadeInLeft">We Build Your Home</h1>
@@ -37,12 +48,15 @@ const CarouselComponent = () => {
       </Carousel.Item>
 
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={im3}
-          alt="Carousel Image"
-          style={{background:"rgba(0, 0, 0, .3)"}}
-        />
+        <div className="carousel-image-container">
+          <img
+            className="d-block w-100"
+            src={im3}
+            alt="Carousel Image"
+            style={{ background: "rgba(0, 0, 0, .3)" }}
+          />
+          <div className="carousel-image-overlay"></div>
+        </div>
         <Carousel.Caption>
           <p className="animated fadeInRight">We Are Trusted</p>
           <h1 className="animated fadeInLeft">For Your Dream Home</h1>
@@ -50,19 +64,22 @@ const CarouselComponent = () => {
       </Carousel.Item>
 
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src={im4}
-          alt="Carousel Image"
-          style={{background:"rgba(0, 0, 0, .3)"}}
-        />
+        <div className="carousel-image-container">
+          <img
+            className="d-block w-100"
+            src={im4}
+            alt="Carousel Image"
+            style={{ background: "rgba(0, 0, 0, .3)" }}
+          />
+          <div className="carousel-image-overlay"></div>
+        </div>
         <Carousel.Caption>
           <p className="animated fadeInRight">Trusted Expertise</p>
           <h1 className="animated fadeInLeft">Building Excellence</h1>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
- );
+  );
 };
 
 export default CarouselComponent;
