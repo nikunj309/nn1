0b70066393afa248224798af482923eea30f6f44
nikunj309 <103521892+nikunj309@users.noreply.github.com>
2024-05-19 21:11:38 +0530

    change whatsapp link

--- src/components/WhatsAppIcon.jsx
+++ src/components/WhatsAppIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import w1 from "../assets/img1/whatsapp.png"
 const WhatsAppIcon = () => {
     const phoneNumber = ' +44 7586479703';
-    const whatsappLink = `https://wa.me/${phoneNumber}`;
+    const whatsappLink = `https://api.whatsapp.com/send/?phone=%2B44 7586479703`;
 
     return (
         <a href={whatsappLink} className="whatsapp-float" target="_blank" rel="noopener noreferrer">
